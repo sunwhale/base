f1e21fc93ac750cade25f70d2e90dfa809ff3398
sunwhale <sunwhale@126.com>
2022-09-29 09:34:04 +0800

    0.0.7.2

--- base/blueprints/main.py
+++ base/blueprints/main.py
@@ -3,8 +3,9 @@
 
 """
 import os
+import json
 
-from flask import abort, render_template, flash, redirect, url_for, current_app, send_from_directory, request, abort, Blueprint
+from flask import jsonify, render_template, flash, redirect, url_for, current_app, send_from_directory, request, abort, Blueprint
 from flask_login import login_required, current_user
 
 
@@ -16,6 +17,21 @@ def index():
     return render_template('main/index.html')
 
 
-@main_bp.route('/test')
+@main_bp.route('/test', methods=['GET', 'POST'])
 def test():
-    abort(400)
+    return render_template('main/test.html')
+
+
+@main_bp.route('/data', methods=['POST'])
+def data():
+    d = request.form['exceldata']
+    with open('data.txt', 'w', encoding='utf-8') as f:
+        json.dump(d, f, ensure_ascii=False)
+    return d
+
+
+@main_bp.route('/readdata', methods=['POST'])
+def readdata():
+    with open('data.txt', 'r', encoding='utf-8') as f:
+        d = json.load(f)
+    return d
