53fbb1bc2191ddf65a971734c1f3d82407e4367d
sunwhale <sunwhale@126.com>
2023-02-27 23:59:08 +0800

    0.1.2.7

---
+++ app.py
@@ -0,0 +1,20 @@
+import webview
+import os
+
+from dotenv import load_dotenv
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
+
+from base import create_app
+app = create_app('production')
+
+
+def resize(window):
+    window.resize(1600, 1200)
+
+
+if __name__ == "__main__":
+    window = webview.create_window('固体推进剂测试', app, min_size=(800, 600))
+    webview.start(resize, window, debug=False)
