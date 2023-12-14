5f22385f666065911216267921bf4c3f57ea7291
sunwhale <sunwhale@126.com>
2023-12-14 17:09:20 +0800

    修改sync

--- base/blueprints/sync.py
+++ base/blueprints/sync.py
@@ -2,8 +2,10 @@
 """
 
 """
-from flask import (Blueprint, jsonify, redirect, render_template, url_for)
+from flask import (Blueprint, jsonify, redirect, render_template, url_for, current_app)
 from flask_login import login_required
+
+from base.utils.dir_status import get_server_uuid
 
 sync_bp = Blueprint('sync', __name__)
 
@@ -11,4 +13,12 @@ sync_bp = Blueprint('sync', __name__)
 @sync_bp.route('/', methods=['GET', 'POST'])
 @login_required
 def index():
-    return render_template('sync/index.html')
+    server_uuid = get_server_uuid(current_app.config['FILE_PATH'])
+    return render_template('sync/index.html', server_uuid=server_uuid)
+
+
+@sync_bp.route('/uuid', methods=['GET', 'POST'])
+@login_required
+def uuid():
+    server_uuid = get_server_uuid(current_app.config['FILE_PATH'])
+    return jsonify(server_uuid)
