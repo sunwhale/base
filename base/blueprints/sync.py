1482c8f8ed989faa3ac1a96022b0a68646416ca0
sunwhale <sunwhale@126.com>
2023-12-18 15:39:34 +0800

    Update sync.py

--- base/blueprints/sync.py
+++ base/blueprints/sync.py
@@ -22,23 +22,49 @@ server_url = 'https://sunjingyu.com'
 server_url = 'http://127.0.0.1:5000'
 
 
-def get_files(module, module_id, module_path, status):
-    status['status'] = 'loading'
-    for sub_dir in sub_dirs(os.path.join(module_path, module_id)):
-        for file_dict in files_in_dir(os.path.join(module_path, sub_dir)):
-            url = server_url + f"/sync/get_file?module={module}&module_id={module_id}&sub_module_id={sub_dir}&filename={file_dict['name']}"
-            response = requests.get(url)
-            status[url] = response.status_code
-    status['status'] = 'done'
+# def get_files(module, module_id, module_path, status):
+#     status['status'] = 'loading'
+#     for sub_dir in sub_dirs(os.path.join(module_path, module_id)):
+#         for file_dict in files_in_dir(os.path.join(module_path, sub_dir)):
+#             url = server_url + f"/sync/get_file?module={module}&module_id={module_id}&sub_module_id={sub_dir}&filename={file_dict['name']}"
+#             response = requests.get(url)
+#             status[url] = response.status_code
+#     status['status'] = 'done'
 
 
-def save_files(module, module_id, resource_dict, status):
+def get_module_path_dict():
+    module_path_dict = {
+        'abaqus': current_app.config['ABAQUS_PATH'],
+        'abaqus_template': current_app.config['ABAQUS_TEMPLATE_PATH'],
+        'virtual': current_app.config['VIRTUAL_PATH'],
+        'experiment': current_app.config['EXPERIMENT_PATH'],
+        'doc': current_app.config['DOC_PATH'],
+        'sheet': current_app.config['SHEET_PATH'],
+        'packing_models': current_app.config['PACKING_MODELS_PATH'],
+        'pyfem': current_app.config['PYFEM_PATH']
+    }
+    return module_path_dict
+
+
+def save_files(module, module_id, module_path_dict, resource_dict, status):
     status['status'] = 'loading'
+    save_path = os.path.join(module_path_dict[module], '20')
+    if not os.path.exists(save_path):
+        os.mkdir(save_path)
     for directory in resource_dict['data']:
+        if not os.path.exists(os.path.join(save_path, directory)):
+            os.mkdir(os.path.join(save_path, directory))
         for url in resource_dict['data'][directory]:
-            response = requests.get(url)
-            status[url] = response.status_code
-            print(response)
+            filename = resource_dict['data'][directory][url][0]
+            file_md5 = resource_dict['data'][directory][url][1]
+            file_local = os.path.join(save_path, directory, filename)
+            if os.path.exists(file_local) and calculate_checksum(file_local) == file_md5:
+                status[url] = 'existed'
+            else:
+                response = requests.get(url)
+                with open(file_local, 'wb') as file:
+                    file.write(response.content)
+                status[url] = 'downloaded'
     status['status'] = 'done'
 
 
@@ -50,17 +76,9 @@ def index():
 
 @sync_bp.route('/resource/<module>/<module_id>', methods=['GET', 'POST'])
 def resource(module, module_id):
-    module_path_dict = {
-        'abaqus': current_app.config['ABAQUS_PATH'],
-        'abaqus_template': current_app.config['ABAQUS_TEMPLATE_PATH'],
-        'virtual': current_app.config['VIRTUAL_PATH'],
-        'experiment': current_app.config['EXPERIMENT_PATH'],
-        'doc': current_app.config['DOC_PATH'],
-        'sheet': current_app.config['SHEET_PATH'],
-        'packing_models': current_app.config['PACKING_MODELS_PATH'],
-        'pyfem': current_app.config['PYFEM_PATH']
-    }
+    module_path_dict = get_module_path_dict()
     module_resource = {'module': module, 'module_id': module_id, 'data': {}}
+    # time.sleep(2)
 
     if module in module_path_dict:
         module_path = module_path_dict[module]
@@ -71,13 +89,13 @@ def resource(module, module_id):
         module_resource['data']['.'] = {}
         for file_dict in files_in_dir(os.path.join(module_path, module_id)):
             url = server_url + f"/sync/get_file?module={module}&module_id={module_id}&filename={file_dict['name']}"
-            module_resource['data']['.'][url] = calculate_checksum(os.path.join(module_path, module_id, file_dict['name']))
+            module_resource['data']['.'][url] = [file_dict['name'], calculate_checksum(os.path.join(module_path, module_id, file_dict['name']))]
 
         for sub_dir in sub_dirs(os.path.join(module_path, module_id)):
             module_resource['data'][sub_dir] = {}
             for file_dict in files_in_dir(os.path.join(module_path, module_id, sub_dir)):
                 url = server_url + f"/sync/get_file?module={module}&module_id={module_id}&sub_module_id={sub_dir}&filename={file_dict['name']}"
-                module_resource['data'][sub_dir][url] = calculate_checksum(os.path.join(module_path, module_id, sub_dir, file_dict['name']))
+                module_resource['data'][sub_dir][url] = [file_dict['name'], calculate_checksum(os.path.join(module_path, module_id, sub_dir, file_dict['name']))]
         return jsonify(module_resource)
     else:
         return jsonify({})
@@ -85,23 +103,20 @@ def resource(module, module_id):
 
 @sync_bp.route('/download/<module>/<module_id>', methods=['GET', 'POST'])
 def download(module, module_id):
-    module_path_dict = {
-        'abaqus': current_app.config['ABAQUS_PATH'],
-        'abaqus_template': current_app.config['ABAQUS_TEMPLATE_PATH'],
-        'virtual': current_app.config['VIRTUAL_PATH'],
-        'experiment': current_app.config['EXPERIMENT_PATH'],
-        'doc': current_app.config['DOC_PATH'],
-        'sheet': current_app.config['SHEET_PATH'],
-        'packing_models': current_app.config['PACKING_MODELS_PATH'],
-        'pyfem': current_app.config['PYFEM_PATH']
-    }
+    module_path_dict = get_module_path_dict()
     if module in module_path_dict:
         module_path = module_path_dict[module]
     else:
         abort(404)
 
     url = server_url + f'/sync/resource/{module}/{module_id}'
-    response = requests.get(url)
+    try:
+        response = requests.get(url, timeout=10)
+    except requests.exceptions.Timeout:
+        abort(408)
+    except requests.exceptions.RequestException as e:
+        abort(500)
+
     resource_dict = json.loads(response.text)
 
     thread_name = f'{module}/{module_id}'
@@ -110,7 +125,7 @@ def download(module, module_id):
         sync_threads[thread_name] = {}
         status = sync_threads[thread_name]
         status['status'] = 'submitted'
-        args = (module, module_id, resource_dict, status)
+        args = (module, module_id, module_path_dict, resource_dict, status)
         thread = threading.Thread(target=save_files, args=args)
         thread.start()
     else:
@@ -119,11 +134,11 @@ def download(module, module_id):
             sync_threads[thread_name] = {}
             status = sync_threads[thread_name]
             status['status'] = 'submitted'
-            args = (module, module_id, resource_dict, status)
+            args = (module, module_id, module_path_dict, resource_dict, status)
             thread = threading.Thread(target=save_files, args=args)
             thread.start()
 
-    return response.text
+    return f'files loading...'
 
 
 @sync_bp.route('/download_status/<module>/<module_id>/', methods=['GET', 'POST'])
@@ -137,17 +152,7 @@ def download_status(module, module_id):
 
 @sync_bp.route('/get_file', methods=['GET', 'POST'])
 def get_file():
-    module_path_dict = {
-        'abaqus': current_app.config['ABAQUS_PATH'],
-        'abaqus_template': current_app.config['ABAQUS_TEMPLATE_PATH'],
-        'virtual': current_app.config['VIRTUAL_PATH'],
-        'experiment': current_app.config['EXPERIMENT_PATH'],
-        'doc': current_app.config['DOC_PATH'],
-        'sheet': current_app.config['SHEET_PATH'],
-        'packing_models': current_app.config['PACKING_MODELS_PATH'],
-        'pyfem': current_app.config['PYFEM_PATH']
-    }
-
+    module_path_dict = get_module_path_dict()
     module = request.args.get('module')
     module_id = request.args.get('module_id')
     sub_module_id = request.args.get('sub_module_id')
