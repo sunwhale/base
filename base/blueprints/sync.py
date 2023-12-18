9ad8dfb284c461be66a0a0072d1be748d488b64a
sunwhale <sunwhale@126.com>
2023-12-18 22:59:26 +0800

    Update sync.py

--- base/blueprints/sync.py
+++ base/blueprints/sync.py
@@ -20,17 +20,17 @@ sync_bp = Blueprint('sync', __name__)
 
 server_url = 'https://sunjingyu.com'
 
-server_url = 'http://127.0.0.1:5000'
+# server_url = 'http://127.0.0.1:5000'
 
 allowed_suffix_dict = {
-    'abaqus': ['inp', 'for', 'json', 'abaqus_msg', 'job_msg'],
-    'abaqus_template': ['inp', 'for', 'json', 'abaqus_msg', 'job_msg'],
-    'virtual': ['inp', 'for', 'json', 'abaqus_msg', 'job_msg'],
-    'experiment': ['*'],
-    'doc': ['*'],
-    'sheet': ['*'],
-    'packing_models': ['*'],
-    'pyfem': ['toml', 'inp', 'msh', 'project_msg', 'job_msg']
+    'abaqus': ['.inp', '.for', '.json', '.abaqus_msg', '.job_msg', '.uuid'],
+    'abaqus_template': ['.inp', '.for', '.json', '.abaqus_msg', '.job_msg', '.uuid'],
+    'virtual': ['.inp', '.for', '.json', '.abaqus_msg', '.job_msg', '.uuid'],
+    'experiment': ['.*'],
+    'doc': ['.*'],
+    'sheet': ['.*'],
+    'packing_models': ['.*'],
+    'pyfem': ['.toml', '.inp', '.msh', '.project_msg', '.job_msg', '.uuid']
 }
 
 
@@ -50,7 +50,10 @@ def get_module_path_dict():
 
 def save_files(module, module_id, module_path_dict, resource_dict, status):
     status['status'] = 'loading'
-    save_path = os.path.join(module_path_dict[module], module_id)
+    module_path = module_path_dict[module]
+    if not os.path.exists(module_path):
+        os.mkdir(module_path)
+    save_path = os.path.join(module_path, str(module_id))
     if not os.path.exists(save_path):
         os.mkdir(save_path)
     for directory in resource_dict['data']:
@@ -84,26 +87,30 @@ def resource(module, module_id):
     if module in module_path_dict:
         module_path = module_path_dict[module]
     else:
-        return jsonify({})
+        return jsonify(None)
 
     sub_module_path = os.path.join(module_path, module_id)
 
     if os.path.exists(sub_module_path):
         module_resource['data']['.'] = {}
         for file_dict in files_in_dir(sub_module_path):
-            url = server_url + f"/sync/get_file?module={module}&module_id={module_id}&filename={file_dict['name']}"
-            file_path = os.path.join(sub_module_path, file_dict['name'])
-            module_resource['data']['.'][url] = [file_dict['name'], calculate_checksum(file_path)]
+            suffix = os.path.splitext(file_dict['name'])[1]
+            if suffix in allowed_suffix_dict[module] or '.*' in allowed_suffix_dict[module]:
+                url = server_url + f"/sync/get_file?module={module}&module_id={module_id}&filename={file_dict['name']}"
+                file_path = os.path.join(sub_module_path, file_dict['name'])
+                module_resource['data']['.'][url] = [file_dict['name'], calculate_checksum(file_path)]
 
         for sub_dir in sub_dirs(sub_module_path):
             module_resource['data'][sub_dir] = {}
             for file_dict in files_in_dir(os.path.join(sub_module_path, sub_dir)):
-                url = server_url + f"/sync/get_file?module={module}&module_id={module_id}&sub_module_id={sub_dir}&filename={file_dict['name']}"
-                file_path = os.path.join(sub_module_path, sub_dir, file_dict['name'])
-                module_resource['data'][sub_dir][url] = [file_dict['name'], calculate_checksum(file_path)]
+                suffix = os.path.splitext(file_dict['name'])[1]
+                if suffix in allowed_suffix_dict[module] or '.*' in allowed_suffix_dict[module]:
+                    url = server_url + f"/sync/get_file?module={module}&module_id={module_id}&sub_module_id={sub_dir}&filename={file_dict['name']}"
+                    file_path = os.path.join(sub_module_path, sub_dir, file_dict['name'])
+                    module_resource['data'][sub_dir][url] = [file_dict['name'], calculate_checksum(file_path)]
         return jsonify(module_resource)
     else:
-        return jsonify({})
+        return jsonify(None)
 
 
 @sync_bp.route('/download/<module>/<server_module_id>/to/<local_module_id>', methods=['GET', 'POST'])
@@ -124,6 +131,9 @@ def download(module, server_module_id, local_module_id):
 
     resource_dict = json.loads(response.text)
 
+    if int(local_module_id) == 0:
+        local_module_id = create_id(module_path)
+
     thread_name = f'{module}/{server_module_id}/{local_module_id}'
 
     if thread_name not in sync_threads:
@@ -143,7 +153,7 @@ def download(module, server_module_id, local_module_id):
             thread = threading.Thread(target=save_files, args=args)
             thread.start()
 
-    return f'files loading...'
+    return str(local_module_id)
 
 
 @sync_bp.route('/download_status/<module>/<server_module_id>/to/<local_module_id>', methods=['GET', 'POST'])
