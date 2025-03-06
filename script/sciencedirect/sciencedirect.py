e91665edbff36ddab8afbe9cbf6a37ecdcfff265
sunwhale <sunwhale@126.com>
2025-03-06 11:36:59 +0800

    Update sciencedirect.py

--- script/sciencedirect/sciencedirect.py
+++ script/sciencedirect/sciencedirect.py
@@ -1,6 +1,7 @@
 import os
 import re
 import time
+import json
 
 import pyhttpx
 from tqdm import tqdm
@@ -8,7 +9,23 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 
 
-def download_bibtex_from_sciencedirect(identifier_value: str) -> None:
+def dump_json(file_path, data, encoding='utf-8'):
+    """
+    Write JSON data to file.
+    """
+    with open(file_path, 'w', encoding=encoding) as f:
+        return json.dump(data, f, ensure_ascii=False)
+
+
+def load_json(file_path, encoding='utf-8'):
+    """
+    Read JSON data from file.
+    """
+    with open(file_path, 'r', encoding=encoding) as f:
+        return json.load(f)
+
+
+def download_bibtex_from_sciencedirect(pii_value: str, bib_path: str = 'bibs') -> None:
     """
     requests库遇到403错误参考：https://blog.csdn.net/SweetHeartHuaZai/article/details/130983179
     url = 'https://doi.org/10.1016/j.jmps.2022.104931'
@@ -18,7 +35,7 @@ def download_bibtex_from_sciencedirect(identifier_value: str) -> None:
     url = 'https://www.sciencedirect.com/sdfe/arp/citingArticles?pii=S0022509622001284&doi=10.1016%2Fj.jmps.2022.104931'
     url = 'https://www.sciencedirect.com/sdfe/arp/pii/S0022509622001284/references?entitledToken=8AC6DBBF7834A727914337C404552FB7C7F31794ACDB3846F7EF09FC50632F2C0D3399C91B1BD982'
     """
-    url = f'https://www.sciencedirect.com/sdfe/arp/cite?pii={identifier_value}&format=text%2Fx-bibtex&withabstract=true'
+    url = f'https://www.sciencedirect.com/sdfe/arp/cite?pii={pii_value}&format=text%2Fx-bibtex&withabstract=true'
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36 Edg/133.0.0.0",
     }
@@ -26,25 +43,42 @@ def download_bibtex_from_sciencedirect(identifier_value: str) -> None:
     response = session.get(url=url, headers=headers, timeout=10, verify=False)
 
     if response.status_code == 200:  # 检查是否成功获取数据
-        with open(f'{identifier_value}.txt', 'w', encoding='utf-8') as f:
+        with open(os.path.join(bib_path, f'{pii_value}.txt'), 'w', encoding='utf-8') as f:
             f.write(response.text)
-        print(f'{identifier_value}.txt is downloaded.')
+        print(f'{pii_value}.txt is downloaded.')
     else:
         print('Failed to retrieve data. Status code:', response.status_code)
 
 
-def get_rendered_source_code_from_sciencedirect(driver, identifier_value: str, sleep_time: int = 5) -> str:
-    driver.get(f'https://www.sciencedirect.com/science/article/pii/{identifier_value}')
+def download_html_code_from_sciencedirect(driver, pii_value: str, sleep_time: int = 5, html_path: str = 'htmls') -> None:
+    driver.get(f'https://www.sciencedirect.com/science/article/pii/{pii_value}')
     time.sleep(sleep_time)
-    rendered_source_code = driver.page_source
+    html_code = driver.page_source
     print(f'get_rendered_source_code_from_sciencedirect: Done')
-    with open(os.path.join('files', f'{identifier_value}.html'), 'w', encoding='utf-8') as f:
-        f.write(rendered_source_code)
-    return rendered_source_code
+    with open(os.path.join(html_path, f'{pii_value}.html'), 'w', encoding='utf-8') as f:
+        f.write(html_code)
 
 
-def find_pdf_url_in_html(html_code: str) -> str:
+def analyze_html_code(pii_value: str, html_path: str = 'htmls', json_path: str = 'jsons') -> None:
+    with open(os.path.join(html_path, f'{pii_value}.html'), 'r', encoding='utf-8') as f:
+        html_code = f.read()
+
     soup = BeautifulSoup(html_code, 'html.parser')
+
+    cited_pii_list = find_cited_pii(soup)
+    ref_pii_list = find_ref_pii(soup)
+    cited_count = find_cited_count(soup)
+
+    data = {
+        'cited_count': cited_count,
+        'cited_pii': cited_pii_list,
+        'ref_pii': ref_pii_list,
+    }
+
+    dump_json(os.path.join(json_path, f'{pii_value}.json'), data, encoding='utf-8')
+
+
+def find_pdf_url(soup: BeautifulSoup) -> str:
     a_block = soup.find('a', {'class': 'link-button accessbar-utility-component accessbar-utility-link link-button-primary link-button-icon-left'})
     if a_block:
         return a_block.get('href')
@@ -52,9 +86,8 @@ def find_pdf_url_in_html(html_code: str) -> str:
         return ''
 
 
-def find_references_pii_in_html(html_code: str) -> list[str]:
+def find_ref_pii(soup: BeautifulSoup) -> list[str]:
     pii_list = []
-    soup = BeautifulSoup(html_code, 'html.parser')
     ol_block = soup.find('ol', {'class': 'references'})
     if ol_block:
         for li in ol_block.find_all('li'):
@@ -69,9 +102,8 @@ def find_references_pii_in_html(html_code: str) -> list[str]:
     return pii_list
 
 
-def find_cited_pii_in_html(html_code: str) -> list[str]:
+def find_cited_pii(soup: BeautifulSoup) -> list[str]:
     pii_list = []
-    soup = BeautifulSoup(html_code, 'html.parser')
     div_block = soup.find('div', {'id': 'section-cited-by'})
     if div_block:
         for a in div_block.find_all('a', {'class': 'anchor anchor-primary'}):
@@ -82,8 +114,7 @@ def find_cited_pii_in_html(html_code: str) -> list[str]:
     return pii_list
 
 
-def find_cited_count_in_html(html_code: str) -> int:
-    soup = BeautifulSoup(html_code, 'html.parser')
+def find_cited_count(soup: BeautifulSoup) -> int:
     div_block = soup.find('div', {'id': 'section-cited-by'})
     if div_block:
         h2_block = div_block.find('h2', {'class': 'u-h4 u-margin-l-ver u-font-serif'})
@@ -102,16 +133,15 @@ def find_cited_count_in_html(html_code: str) -> int:
 
 
 if __name__ == '__main__':
+    pii_value = 'S0022509622001284'
     # driver = webdriver.Edge()
-    identifier_value = 'S0022509623000972'
-    # rendered_source_code = get_rendered_source_code_from_sciencedirect(driver, identifier_value)
-    with open(os.path.join('files', f'{identifier_value}.html'), 'r', encoding='utf-8') as f:
-        rendered_source_code = f.read()
+    # rendered_source_code = download_html_code_from_sciencedirect(driver, pii_value)
+    analyze_html_code('S0022509622001284')
 
     # identifier_value_list = find_references_identifier_value_in_html(rendered_source_code)
-    print(find_cited_count_in_html(rendered_source_code))
-    print(find_cited_pii_in_html(rendered_source_code))
-    print(find_references_pii_in_html(rendered_source_code))
+    # print(find_cited_count_in_html(rendered_source_code))
+    # print(find_cited_pii_in_html(rendered_source_code))
+    # print(find_references_pii_in_html(rendered_source_code))
     # for identifier_value in identifier_value_list:
     #     download_bibtex_from_sciencedirect(identifier_value)
     # driver.quit()
