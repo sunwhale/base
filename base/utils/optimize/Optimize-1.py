ffe47180efac9df4af63815b472668fdf8768db8
sunwhale <sunwhale@126.com>
2025-11-06 09:01:27 +0800

    Update Optimize-1.py

--- base/utils/optimize/Optimize-1.py
+++ base/utils/optimize/Optimize-1.py
@@ -14,6 +14,8 @@ from pathlib import Path
 from typing import Optional
 
 import colorlog
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
