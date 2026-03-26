7847aa5ad62a7e68539feccfd6cd3e42d19a2e8b
sunwhale <sunwhale@126.com>
2026-03-26 20:58:08 +0800

    增加流程2

--- base/blueprints/flow.py
+++ base/blueprints/flow.py
@@ -11,7 +11,7 @@ from flask_login import current_user
 from markupsafe import Markup
 
 from base.decorators import permission_required
-from base.forms.flow import UploadForm, FlowForm, JobForm, F1From
+from base.forms.flow import UploadForm, FlowForm, JobForm, F1Form, F2Form
 from base.utils.abaqus.Preproc import Preproc
 from base.utils.common import make_dir, dump_json, load_json
 from base.utils.dir_status import (create_id, files_in_dir, flows_detail, materials_detail, projects_detail, file_time)
@@ -128,7 +128,7 @@ def f1():
 
     upload_form = UploadForm(prefix='upload')
     job_form = JobForm(prefix='job')
-    form = F1From()
+    form = F1Form()
 
     form.step_file_name.choices = step_file_name_list
     form.material_tool.choices = material_list
@@ -331,6 +331,245 @@ def f1():
                     status[key] = 'None'
 
         return render_template('flow/f1.html', flow_id=flow_id, status=status, form=form, upload_form=upload_form, job_form=job_form)
+    else:
+        abort(404)
+
+
+@flow_bp.route('/2', methods=['GET', 'POST'])
+@permission_required('FLOW')
+def f2():
+    flow_id = 2
+    flows_path = current_app.config['FLOW_PATH']
+    flow_path = os.path.join(flows_path, str(flow_id))
+    setting_file = os.path.join(flow_path, 'setting.json')
+    job_form_message_file = os.path.join(flow_path, '.job_form_msg')
+
+    materials_path = current_app.config['MATERIAL_PATH']
+    material_dict = materials_detail(materials_path)
+    material_list = []
+    for material in material_dict['data']:
+        material_list.append('%s_%s' % (material['material_id'], material['name']))
+
+    abaqus_path = current_app.config['ABAQUS_PATH']
+    project_dict = projects_detail(abaqus_path)
+    project_list = []
+    for project in project_dict['data']:
+        project_list.append('%s_%s' % (project['project_id'], project['name']))
+
+    files = files_in_dir(flow_path)
+    step_file_name_list = []
+    tool_path_file_name_list = []
+    for file in files:
+        if file['name'].endswith('.stp'):
+            step_file_name_list.append(file['name'])
+        elif file['name'].endswith('.txt'):
+            tool_path_file_name_list.append(file['name'])
+
+    upload_form = UploadForm(prefix='upload')
+    job_form = JobForm(prefix='job')
+    form = F2Form()
+
+    form.step_file_name.choices = step_file_name_list
+    form.material_tool.choices = material_list
+    form.material_plane.choices = material_list
+    form.material_interaction.choices = material_list
+    form.tool_path_file_name.choices = tool_path_file_name_list
+
+    job_form.project_id.choices = project_list
+
+    if upload_form.submit.data and upload_form.validate():
+        f = upload_form.filename.data
+        f.save(os.path.join(flow_path, f.filename))
+        flash('上传文件%s成功。' % f.filename, 'success')
+        return redirect(url_for('flow.f1', flow_id=flow_id, form=form, upload_form=upload_form, job_form=job_form))
+
+    if job_form.submit.data and job_form.validate():
+        project_id = job_form.project_id.data.split('_')[0]
+        abaqus_path = current_app.config['ABAQUS_PATH']
+        project_path = os.path.join(abaqus_path, str(project_id))
+        overwrite = job_form.overwrite.data
+        if overwrite:
+            job_id = job_form.job_id.data
+            job_path = os.path.join(project_path, str(job_id))
+            if not os.path.exists(job_path):
+                flash('不存在ABAQUS作业%s-%s！' % (project_id, job_id), 'danger')
+                return redirect(url_for('flow.f1', flow_id=flow_id, form=form, upload_form=upload_form, job_form=job_form))
+        else:
+            job_id = create_id(project_path)
+            job_path = os.path.join(project_path, str(job_id))
+            make_dir(job_path)
+
+        job_message = {
+            'job': job_form.job.data,
+            'user': job_form.user.data,
+            'cpus': job_form.cpus.data,
+            'descript': job_form.descript.data
+        }
+        job_message_file = os.path.join(job_path, '.job_msg')
+        dump_json(job_message_file, job_message)
+
+        files = files_in_dir(flow_path)
+        for file in files:
+            shutil.copy(os.path.join(flow_path, file['name']), os.path.join(job_path, file['name']))
+        flash(
+            Markup('从流程1导出文件到<a href="%s">ABAQUS项目%s-%s</a>成功。') % (
+                url_for('abaqus.view_job', project_id=project_id, job_id=job_id), project_id, job_id),
+            'success'
+        )
+
+        job_form_message = {
+            'project_id': job_form.project_id.data,
+            'job_id': job_form.job_id.data,
+            'overwrite': job_form.overwrite.data,
+            'job': job_form.job.data,
+            'user': job_form.user.data,
+            'cpus': job_form.cpus.data,
+            'descript': job_form.descript.data
+        }
+        dump_json(job_form_message_file, job_form_message)
+
+        return redirect(url_for('flow.f1', flow_id=flow_id, form=form, upload_form=upload_form, job_form=job_form))
+
+    if os.path.exists(job_form_message_file):
+        job_form_message = load_json(job_form_message_file)
+        job_form.project_id.data = job_form_message['project_id']
+        job_form.job_id.data = job_form_message['job_id']
+        job_form.overwrite.data = job_form_message['overwrite']
+        job_form.job.data = job_form_message['job']
+        job_form.user.data = job_form_message['user']
+        job_form.cpus.data = job_form_message['cpus']
+        job_form.descript.data = job_form_message['descript']
+
+    if form.submit.data and form.validate():
+        message = {
+            'tool_option': form.tool_option.data,
+            'r1': form.r1.data,
+            'r2': form.r2.data,
+            'n': form.n.data,
+            'length': form.length.data,
+            'pitch': form.pitch.data,
+            'step_file_name': form.step_file_name.data,
+            'tool_ref_point_x': form.tool_ref_point_x.data,
+            'tool_ref_point_y': form.tool_ref_point_y.data,
+            'tool_ref_point_z': form.tool_ref_point_z.data,
+            'tool_seed_size': form.tool_seed_size.data,
+            'x_length_of_plane': form.x_length_of_plane.data,
+            'y_length_of_plane': form.y_length_of_plane.data,
+            'z_length_of_plane': form.z_length_of_plane.data,
+            'plane_seed_size': form.plane_seed_size.data,
+            'x_shift_of_tool': form.x_shift_of_tool.data,
+            'y_shift_of_tool': form.y_shift_of_tool.data,
+            'z_shift_of_tool': form.z_shift_of_tool.data,
+            'material_tool': form.material_tool.data,
+            'material_plane': form.material_plane.data,
+            'tool_rotation_speed': form.tool_rotation_speed.data,
+            'tool_shift_speed': form.tool_shift_speed.data,
+            'temperature_tool_z1': form.temperature_tool_z1.data,
+            'temperature_tool_init': form.temperature_tool_init.data,
+            'temperature_plane_init': form.temperature_plane_init.data,
+            'tool_path_type': form.tool_path_type.data,
+            'square_wave_width': form.square_wave_width.data,
+            'square_wave_height': form.square_wave_height.data,
+            'square_wave_depth': form.square_wave_depth.data,
+            'square_wave_head_shift': form.square_wave_head_shift.data,
+            'square_wave_tail_shift': form.square_wave_tail_shift.data,
+            'square_wave_cycles': form.square_wave_cycles.data,
+            'square_wave_layers': form.square_wave_layers.data,
+            'drill_depth': form.drill_depth.data,
+            'tool_path_file_name': form.tool_path_file_name.data,
+            'material_interaction': form.material_interaction.data,
+            'output_variables': form.output_variables.data,
+            'output_numIntervals': form.output_numIntervals.data,
+            'timeIncrementationMethod': form.timeIncrementationMethod.data,
+            'userDefinedInc': form.userDefinedInc.data,
+            'step_time': form.step_time.data,
+            'mass_scaling_factor': form.mass_scaling_factor.data,
+            'linearBulkViscosity': form.linearBulkViscosity.data,
+            'quadBulkViscosity': form.quadBulkViscosity.data,
+        }
+        dump_json(setting_file, message)
+
+        material_id = int(form.material_tool.data.split('_')[0])
+        material_json_path = os.path.join(materials_path, str(material_id), 'material.json')
+        shutil.copy(material_json_path, os.path.join(flow_path, 'material_tool.json'))
+        flash('从材料导入文件%s成功。' % 'material_tool.json', 'success')
+
+        material_id = int(form.material_plane.data.split('_')[0])
+        material_json_path = os.path.join(materials_path, str(material_id), 'material.json')
+        shutil.copy(material_json_path, os.path.join(flow_path, 'material_plane.json'))
+        flash('从材料导入文件%s成功。' % 'material_plane.json', 'success')
+
+        material_id = int(form.material_interaction.data.split('_')[0])
+        material_json_path = os.path.join(materials_path, str(material_id), 'material.json')
+        shutil.copy(material_json_path, os.path.join(flow_path, 'material_interaction.json'))
+        flash('从材料导入文件%s成功。' % 'material_interaction.json', 'success')
+
+    if os.path.exists(setting_file):
+        try:
+            message = load_json(setting_file)
+            form.tool_option.data = message['tool_option']
+            form.r1.data = message['r1']
+            form.r2.data = message['r2']
+            form.n.data = message['n']
+            form.length.data = message['length']
+            form.pitch.data = message['pitch']
+            form.step_file_name.data = message['step_file_name']
+            form.tool_ref_point_x.data = message['tool_ref_point_x']
+            form.tool_ref_point_y.data = message['tool_ref_point_y']
+            form.tool_ref_point_z.data = message['tool_ref_point_z']
+            form.tool_seed_size.data = message['tool_seed_size']
+            form.x_length_of_plane.data = message['x_length_of_plane']
+            form.y_length_of_plane.data = message['y_length_of_plane']
+            form.z_length_of_plane.data = message['z_length_of_plane']
+            form.plane_seed_size.data = message['plane_seed_size']
+            form.x_shift_of_tool.data = message['x_shift_of_tool']
+            form.y_shift_of_tool.data = message['y_shift_of_tool']
+            form.z_shift_of_tool.data = message['z_shift_of_tool']
+            form.material_tool.data = message['material_tool']
+            form.material_plane.data = message['material_plane']
+            form.material_interaction.data = message['material_interaction']
+            form.tool_rotation_speed.data = message['tool_rotation_speed']
+            form.tool_shift_speed.data = message['tool_shift_speed']
+            form.temperature_tool_z1.data = message['temperature_tool_z1']
+            form.temperature_tool_init.data = message['temperature_tool_init']
+            form.temperature_plane_init.data = message['temperature_plane_init']
+            form.tool_path_type.data = message['tool_path_type']
+            form.square_wave_width.data = message['square_wave_width']
+            form.square_wave_height.data = message['square_wave_height']
+            form.square_wave_depth.data = message['square_wave_depth']
+            form.square_wave_head_shift.data = message['square_wave_head_shift']
+            form.square_wave_tail_shift.data = message['square_wave_tail_shift']
+            form.square_wave_cycles.data = message['square_wave_cycles']
+            form.square_wave_layers.data = message['square_wave_layers']
+            form.drill_depth.data = message['drill_depth']
+            form.tool_path_file_name.data = message['tool_path_file_name']
+            form.output_variables.data = message['output_variables']
+            form.output_numIntervals.data = message['output_numIntervals']
+            form.timeIncrementationMethod.data = message['timeIncrementationMethod']
+            form.userDefinedInc.data = message['userDefinedInc']
+            form.step_time.data = message['step_time']
+            form.mass_scaling_factor.data = message['mass_scaling_factor']
+            form.linearBulkViscosity.data = message['linearBulkViscosity']
+            form.quadBulkViscosity.data = message['quadBulkViscosity']
+        except KeyError:
+            pass
+
+    if os.path.exists(flow_path):
+        msg_file = os.path.join(flow_path, '.flow_msg')
+        status = {}
+        if os.path.exists(msg_file):
+            status['flow_id'] = flow_id
+            try:
+                with open(msg_file, 'r', encoding='utf-8') as f:
+                    message = json.load(f)
+                status['name'] = message['name']
+                status['flow_time'] = file_time(msg_file)
+                status['descript'] = message['descript']
+            except (FileNotFoundError, KeyError):
+                for key in ['name', 'flow_time', 'descript']:
+                    status[key] = 'None'
+
+        return render_template('flow/f2.html', flow_id=flow_id, status=status, form=form, upload_form=upload_form, job_form=job_form)
     else:
         abort(404)
 
