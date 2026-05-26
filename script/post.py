805b2cb90871fa02a5d4fc821271bf3cfdb23ae6
sunwhale <sunwhale@126.com>
2026-05-26 12:04:44 +0800

    Update post.py

--- script/post.py
+++ script/post.py
@@ -48,6 +48,8 @@ def load_region_data(npz_file, regions):
         odb_data[r]['regionType'] = data[r]['regionType']
         odb_data[r]['fieldOutputs'] = {}
 
+        # print(region_nodeLabels[9])
+
         # stress = np.array(data[r]['fieldOutputs']['S']['values'])
         # try:
         #     strain = np.array(data[r]['fieldOutputs']['E']['values'])
@@ -55,6 +57,8 @@ def load_region_data(npz_file, regions):
         #     strain = np.array(data[r]['fieldOutputs']['LE']['values'])
 
         u = np.array(data[r]['fieldOutputs']['U']['values'])
+
+        # print(u[:,9,:])
 
         out_data[r] = {}
         out_data[r]['step_time'] = step_time
@@ -161,6 +165,23 @@ def normal_component(displacement, a, b, c, d=None):
     return normal_part
 
 
+def distance(point1, point2):
+    """
+    计算两个点之间的欧氏距离。
+
+    参数:
+        point1, point2: 点的坐标，可以是列表或元组，如 (x1, y1) 或 (x1, y1, z1)
+
+    返回:
+        两点之间的欧氏距离（浮点数）
+    """
+    if len(point1) != len(point2):
+        raise ValueError("两个点的维度必须相同")
+
+    squared_diff_sum = sum((p1 - p2) ** 2 for p1, p2 in zip(point1, point2))
+    return math.sqrt(squared_diff_sum)
+
+
 def normal_scalar_projection(displacement, a, b, c, d=None):
     """
     计算位移向量在平面法向上的有符号投影长度。
@@ -189,8 +210,8 @@ jobs_status = load_status('https://www.sunjingyu.com:8010/abaqus/project_jobs_st
 
 job_ids = [8]
 
-set_a = 'BLOCK-2-5.SET-FACES-INSULATION-GLUE-A-T1'
-set_b = 'BLOCK-2-6.SET-FACES-INSULATION-GLUE-A-T-1'
+set_a = 'BLOCK-2-1.SET-FACES-INSULATION-GLUE-A-T1'
+set_b = 'BLOCK-2-2.SET-FACES-INSULATION-GLUE-A-T-1'
 
 for job_id in job_ids:
     job_status = jobs_status[job_id]
@@ -220,17 +241,26 @@ for step in order:
     # 更新偏移量为当前step最后一个时间点（即当前step结束的绝对时间）
     offset = shifted[-1]
 
+print(coords_a[9, :])
+print(coords_b[9, :])
+
 cyl_coords_a = cart2cyl(coords_a)
 cyl_coords_b = cart2cyl(coords_b)
+
+print(cyl_coords_a[9, :])
+print(cyl_coords_b[9, :])
 
 u_a = np.swapaxes(u_a, 0, 1)
 u_b = np.swapaxes(u_b, 0, 1)
 
+print(u_a[9, 82, :])
+print(u_b[9, 82, :])
+
 print(u_a.shape, coords_a.shape)
 u_max = []
 
-for nt in range(0, 83):
-# for nt in [80]:
+# for nt in range(0, 83):
+for nt in [82]:
     a, b, c, d = fit_plane_lsq(coords_a + u_a[:, nt, :])
     # print(f"拟合平面: {a:.4f}*x + {b:.4f}*y + {c:.4f}*z + {d:.4f} = 0")
     # print(f"法向量模长: {np.sqrt(a * a + b * b + c * c):.6f}")
@@ -240,6 +270,7 @@ for nt in range(0, 83):
 
     u = linear_interp(list(zip(cyl_coords_b[:, 0], cyl_coords_b[:, 2]))) - u_b_normal
     u_max.append(u.max())
+    # print(u)
     # x = cyl_coords_a[:,0]
     # y = cyl_coords_a[:,2]
     # z = u
@@ -248,5 +279,5 @@ for nt in range(0, 83):
     # plt.colorbar(label='z')
     # plt.show()
 
-plt.plot(total_time, u_max)
-plt.show()
+# plt.plot(total_time, u_max)
+# plt.show()
