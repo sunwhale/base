37e567c273c36a72c76393bc6de5107b92519c9e
sunwhale <sunwhale@126.com>
2026-08-09 04:09:19 +0800

    增加并行科技智算云服务器配置文件

---
+++ conf/paratera/jupyter/jupyter_lab_config.py
@@ -0,0 +1,6 @@
+c = get_config()
+c.ServerApp.allow_origin = '*'
+c.ServerApp.allow_remote_access = True
+c.ServerApp.ip = '*'
+c.ServerApp.open_browser = False
+c.ServerApp.notebook_dir = '/home/ubuntu'
