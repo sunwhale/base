ae97bb504394d06c47e3e0a97f83fb5b49a458bd
sunwhale <sunwhale@126.com>
2026-08-10 17:18:46 +0800

    更新conf文件

--- conf/paratera/jupyter/jupyter_lab_config.py
+++ conf/paratera/jupyter/jupyter_lab_config.py
@@ -3,4 +3,4 @@ c.ServerApp.allow_origin = '*'
 c.ServerApp.allow_remote_access = True
 c.ServerApp.ip = '*'
 c.ServerApp.open_browser = False
-c.ServerApp.notebook_dir = '/home/ubuntu'
+c.ServerApp.notebook_dir = '/www/base'
